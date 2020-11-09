1e3840a97821a3c4db8dfdf68083556d53552fa0
Alanna Bouloy <lanihope@gmail.com>
2020-11-09 13:36:42 -0500

    added seed file

---
+++ seeds/seed.dnd-campaign-manager-tables.sql
@@ -0,0 +1,18 @@
+BEGIN;
+
+TRUNCATE
+    users
+RESTART IDENTITY CASCADE;
+
+INSERT INTO users (username, user_password, first_name, last_name, email)
+VALUES
+    ('mattmercer', 'pumatrocks', 'Matthew', 'Mercer', 'mmercer@email.com'),
+    ('marishanosleevesray', 'sleevesarebullshit', 'Marisha', 'Ray', 'mray@email.com'),
+    ('laurajests', 'travelerismycopilot', 'Laura', 'Bailey', 'lbailey@email.com'),
+    ('theancientone', 'longmayhereign', 'Talieson', 'Jaffe', 'tjaffe@email.com'),
+    ('thewinemom', 'nott-the-brave', 'Sam', 'Riegel', 'sriegel@email.com'),
+    ('tragicbackstoryliam', 'calebcries', 'Liam', 'O Brien', 'lobrien@email.com'),
+    ('traviswillingham', 'cowboysrule', 'Travis', 'Willingham', 'twillingham@email.com'),
+    ('ashleyjohnson', 'harpsandflowers', 'Ashley', 'Johnson', 'ajohnson@email.com'),
+    ('notacabbage', 'no1puphenry', 'Brian W.', 'Foster', 'bwfoster@email.com');
+
