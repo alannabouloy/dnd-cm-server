45770f9aa5fdb7a02ac15f3228bd56168381894c
Alanna Bouloy <lanihope@gmail.com>
2020-11-18 18:37:50 -0500

    added further security to password authentication

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const AuthService = require('../auth/auth-service')
+
 function requireAuth(req, res, next){
     const authToken = req.get('Authorization') || ''
 
@@ -9,24 +12,34 @@ function requireAuth(req, res, next){
     } else {
         basicToken = authToken.slice('basic'.length, authToken.length)
     }
-    const [tokenUserName, tokenPassword] = Buffer
-        .from(basicToken, 'base64')
-        .toString()
-        .split(':')
+
+    const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
+        
 
     if(!tokenUserName || !tokenPassword) {
         return res.status(401).json({ error: 'Unauthorized request' })
     }
 
-    req.app.get('db')('users')
-        .where({ username : tokenUserName})
-        .first()
+    AuthService.getUserWithUserName(
+        req.app.get('db'),
+        tokenUserName
+    )
         .then(user => {
-            if(!user || user.user_password !== tokenPassword) {
+            if(!user) {
                 return res.status(401).json({ error: 'Unauthorized request'})
             }
-            req.user = user
-            next()
+
+            return bcrypt.compare(tokenPassword, user.user_password)
+                .then(passwordMatch => {
+                    if(!passwordMatch){
+                        return res
+                            .status(401)
+                            .json({error : `Unauthorized request`})
+                    }
+
+                    req.user = user
+                    next()
+                })
         })
         .catch(next)
 }
