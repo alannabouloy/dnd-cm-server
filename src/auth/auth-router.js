57f987a7d48fdad1e2dcb7eea2a9396cf1d36296
Alanna Bouloy <lanihope@gmail.com>
2021-02-09 17:13:34 -0500

    reconfigured auth router for async/await

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -5,9 +5,10 @@ const authRouter = express.Router()
 const jsonBodyParser = express.json()
 
 authRouter
-    .post('/login', jsonBodyParser, (req, res, next) => {
+    .post('/login', jsonBodyParser, async (req, res, next) => {
         const { username, user_password } = req.body
         const loginUser = {username, user_password}
+        const db = req.app.get('db')
 
         for(const [key, value] of Object.entries(loginUser)){
             if(value == null){
@@ -18,36 +19,44 @@ authRouter
                     })
             }
         }
-        AuthService.getUserWithUserName(
-            req.app.get('db'),
-            loginUser.username
-        )
-        .then(dbUser => {
-            if (!dbUser){
+
+        try {
+            //set user from database
+            const dbUser = await AuthService.getUserWithUserName(db, loginUser.username)
+
+            if(!dbUser) {
+                return res
+                    .status(400)
+                    .json({error: 'Incorrect username or user_password'})
+            }
+
+            //compare user_password to password given
+            const compareMatch = await AuthService.comparePasswords(loginUser.user_password, dbUser.user_password)
+
+            if(!compareMatch){
                 return res
                     .status(400)
                     .json({
-                        error: 'Incorrect username or user_password',
+                        error: 'Incorrect username or user_password'
                     })
             }
-            return AuthService.comparePasswords(loginUser.user_password, dbUser.user_password)
-                .then(compareMatch => {
-                    if(!compareMatch){
-                        return res
-                            .status(400)
-                            .json({
-                                error: 'Incorrect username or user_password',
-                            })
-                    }
-                    const sub = dbUser.username
-                    const payload = {user_id: dbUser.id}
-                    const token = AuthService.createJwt(sub, payload)
-                    res.json({
-                        authToken: token,
-                    })
-                })
-        })
-        .catch(next)
+
+            //create JWT
+
+            const sub = dbUser.username
+            const payload = { user_id: dbUser.id }
+            const token = AuthService.createJwt(sub, payload)
+
+            res.json({
+                authToken: token,
+            })
+
+            next()
+
+
+        } catch(error){
+            next(error)
+        }
     })
 
 module.exports = authRouter
