dcf5ec89f03a72f6f66b7c12aa7f19dc80aaf639
Alanna Bouloy <lanihope@gmail.com>
2021-02-23 17:41:50 -0500

    added POST new tag

--- src/tags/tags-router.js
+++ src/tags/tags-router.js
@@ -3,6 +3,8 @@ const TagsService = require('./tags-service')
 const { requireAuth } = require('../middleware/jwt-auth')
 const path = require('path')
 const xss = require('xss')
+const helpers = require('../helpers')
+const NotesService = require('../notes/notes-service')
 
 const tagsRouter = express.Router()
 const jsonParser = express.json()
@@ -10,7 +12,7 @@ const jsonParser = express.json()
 function sanitizeTag(tag){
     tag = {
         ...tag,
-        name: xss(tag.tag_name),
+        tag_name: xss(tag.tag_name),
     }
 
     return tag
@@ -29,6 +31,66 @@ tagsRouter
             res
                 .json(result)
 
+            next()
+        } catch(error){
+            next(error)
+        }
+    })
+    .post(jsonParser, async(req, res, next) => {
+        const db = req.app.get('db')
+        const { tag_name, note } = req.body
+        const admin = req.user.id
+        let newTag = {
+            tag_name,
+            note,
+            admin
+        }
+        //needs to validate that 'name' and 'noteId' are included and valid
+        try {
+            let error = ''
+
+            error = helpers.validateKeys(newTag, [
+                'tag_name',
+                'note',
+                'admin',
+            ])
+
+            if(error){
+                return res.status(400).json(error)
+            }
+
+            error = helpers.validateType(tag_name, 'string')
+            
+            if(error){
+                return res.status(400).json(error)
+            }
+
+            error = helpers.validateType(note, 'number')
+            if(error){
+                return res.status(400).json(error)
+            }
+            
+            error = helpers.validateType(admin, 'number')
+            if(error){
+                return res.status(400).json(error)
+            }
+
+            const hasNote =  await NotesService.getNoteById(db, note)
+
+            if(!hasNote){
+                return res
+                    .status(404)
+                    .json({error: 'Note Not Found'})
+            }
+
+            newTag = sanitizeTag(newTag)
+
+            const added = await TagsService.addNewTag(db, admin, newTag)
+
+            res
+                .status(201)
+                .location(path.posix.join(req.originalUrl, `/${added.id}`))
+                .json(added)
             next()
         } catch(error){
             next(error)
